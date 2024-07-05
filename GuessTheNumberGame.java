b169ef1f8c385976ac2f6ae9b9ed443969682d1a
mohanapriya-g-2003 <167018457+mohanapriya-g-2003@users.noreply.github.com>
2024-07-05 19:37:08 +0530

    Add files via upload

---
+++ GuessTheNumberGame.java
@@ -0,0 +1,38 @@
+package com.guessgame;  
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumberGame {
+    public static void main(String[] args) {
+
+
+        Random random = new Random();
+        int numberToGuess = random.nextInt(100) + 1;
+        
+        Scanner scanner = new Scanner(System.in);
+        int userGuess = 0;
+
+        System.out.println("Welcome to the Guess the Number game!");
+        System.out.println("I have selected a number between 1 and 100. Can you guess it?");
+        
+        
+        while (userGuess != numberToGuess) {
+            
+            System.out.print("Enter your guess: ");
+            userGuess = scanner.nextInt();
+            
+        
+            if (userGuess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (userGuess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You guessed the correct number.");
+            }
+            
+        }
+        
+        scanner.close();
+    }
+}
